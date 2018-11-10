867eeeeb1ff92a24eb125eeacd1294c7ad4a2b99
Eduardo Kramer <duhlaguna@gmail.com>
2018-11-10 14:38:03 -0200

    Adicionar arquivos de projeto.

---
+++ TripTracker.BackService/Models/Trip.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripTracker.BackService.Models
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Destination of the trip
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Description of the trip
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Date of the start of the trip
+        /// </summary>
+        public DateTimeOffset StartDate { get; set; }
+
+        /// <summary>
+        /// Date of the end of the trip
+        /// </summary>
+        public DateTimeOffset EndDate { get; set; }
+    }
+}
---
+++ TripTracker.BackService/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace TripTracker.BackService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //  Transient
+            //  services is our dependency injection
+            //  3 diferent scopes levels for our DI container:
+            //  -Transient: Todas as vezes que for requisitado, crie uma nova
+            //  -Scoped: Criada uma vez por cada pedido HTTP, mas em cada novo pedido HTTP, uma nova é criada
+            //  -Singleton: Apenas criada uma vez em, uma unica vez em todo o aplicativo
+
+            services.AddTransient<Models.Repository>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(options =>
+                options.SwaggerDoc("v1", new Info { Title = "Trip Tracker", Version = "v1" })
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            app.UseSwagger();
+
+            if (env.IsDevelopment() || env.IsStaging())
+            {
+                app.UseSwaggerUI(options =>
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Trip Tracker v1")
+                );
+            }
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
